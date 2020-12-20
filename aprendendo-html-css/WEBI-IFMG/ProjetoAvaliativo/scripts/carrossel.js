58a99706cf05cba75cd414fa4dd6e2f042cefcba
Arthur Terozendi <arthurterozendi@gmail.com>
2020-12-20 17:48:41 -0300

    projeto final v2.0

---
+++ aprendendo-html-css/WEBI-IFMG/ProjetoAvaliativo/scripts/carrossel.js
@@ -0,0 +1,18 @@
+var img = ['imgCarrossel.png', 'imgCarrossel2.png', 'imgCarrossel3.png']
+var imgCarrosselAtual = 0;
+function girarCarrosselPrev() {
+    if (imgCarrosselAtual == 0) {
+        imgCarrosselAtual = 2;
+    } else {
+        imgCarrosselAtual--;
+    }
+    document.getElementById("carrossel").src = `img/${img[imgCarrosselAtual]}`
+}
+function girarCarrosselNext() {
+    if (imgCarrosselAtual == 2) {
+        imgCarrosselAtual = 0;
+    } else {
+        imgCarrosselAtual++;
+    }
+    document.getElementById("carrossel").src = `img/${img[imgCarrosselAtual]}`
+}
