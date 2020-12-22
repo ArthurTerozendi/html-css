a433f61850732d636fbcfd201ae518540188f0a7
Arthur Terozendi <arthurterozendi@gmail.com>
2020-12-22 19:24:54 -0300

    projeto final v3.1

--- aprendendo-html-css/WEBI-IFMG/ProjetoAvaliativo/scripts/escola.js
+++ aprendendo-html-css/WEBI-IFMG/ProjetoAvaliativo/scripts/escola.js
@@ -4,3 +4,17 @@ function mudarFiltro(id) {
 function aplicarGrayScale(id) {
     document.getElementById(`${id}`).style.filter = "grayscale(100%)";
 }
+
+function zoomModal(id) {
+    
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById(id).src;
+    
+    modal.style.display = "block";
+    document.getElementById("imgModal").src = img;
+    window.onclick = event => {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
